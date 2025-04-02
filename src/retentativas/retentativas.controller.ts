8c6dda209420b7771875f2b453bcd264c9b29afc
gravetogeek <edusrm11@gmail.com>
2025-04-01 22:44:24 -0300

    recycle: refactor: Refatoração do módulo retentativas

--- src/retentativas/retentativas.controller.ts
+++ src/retentativas/retentativas.controller.ts
@@ -2,18 +2,13 @@ import {Controller,Post} from '@nestjs/common'
 import {Body} from '@nestjs/common/decorators/http/route-params.decorator'
 import {RetentativasService} from './retentativas.service'; // Adjust the path as necessary
 
-@Controller('retentativas')
+@Controller()
 export class RetentativasController {
     constructor(private readonly retentativasService: RetentativasService) {}
 
-    @Post('realizar')
+    @Post('realizarRetentativa')
     async realizarRetentativa(@Body() dados: any) {
         console.log('realizarRetentativa');
         return this.retentativasService.realizarRetentativa(dados);
-    }
-    @Post('agendar')
-    async agendarProximaRetentativa(@Body() dados: any) {
-        console.log('agendarProximaRetentativa');
-        return this.retentativasService.agendarProximaRetentativa(dados);
     }
 }
--- src/retentativas/retentativas.service.ts
+++ src/retentativas/retentativas.service.ts
@@ -1,59 +1,35 @@
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import {Injectable} from '@nestjs/common'
-import {EventbridgeService} from '../eventbridge/eventbridge.service'
-
+// import util from 'node:util'
+import {EventbridgeService} from 'src/eventbridge/eventbridge.service'
 @Injectable()
 export class RetentativasService {
     constructor(private eventbridgeService: EventbridgeService) { }
 
-    async realizarRetentativa(dados: any) {
+    async realizarRetentativa(dados: { [key: string]: any; tentativa?: number }) {
         try {
-            console.log(process.env.API_GET_CUSTOMER_URL)
-            if (!process.env.API_GET_CUSTOMER_URL) {
-                throw new Error('API_GET_CUSTOMER_URL is not defined in the environment variables');
-            }
-            const url = new URL(process.env.API_GET_CUSTOMER_URL);
-            const response = await fetch(url.toString());
+            const url = new URL('http://localhost:4566/restapis/ngdgjtymf6/dev/get_customer');
+            console.log(url.toString())
+            console.log({dados})
+            const response = await fetch(url,{
+                method:"POST",
+                body: JSON.stringify({
+                    id: dados.id
+                })
+            });
+            console.log(await response.json())
             if (!response.ok) {
                 throw new Error(`HTTP error! status: ${response.status}`);
             }
-            const responseData = await response.json();
+            const responseData: Record<string, unknown> = await response.json();
             console.log('Retentativa bem-sucedida:', responseData);
-            //enviar HSM de sucesso
+            console.log("HSM Sucesso")
+            return responseData
         } catch (error) {
             console.error('Retentativa falhou:', error.message);
-            await this.agendarProximaRetentativa(dados);
-        }
-    }
-
-    async agendarProximaRetentativa(dados: any) {
-        console.log('agendarProximaRetentativa', dados);
-        if (dados.tentativa < 3) {
-            dados.tentativa++;
-            let delay = 0;
-
-            if (dados.tentativa === 2) {
-                delay = 60 * 60 * 1000; // 1 hora em milissegundos
-            } else if (dados.tentativa === 3) {
-                delay = 4 * 60 * 60 * 1000; // 4 horas em milissegundos
-            } else {
-                delay = 5 * 60 * 1000; // 5 minutos em milissegundos, para a primeira retentativa.
-            }
-
-            // Calcula o tempo futuro para a próxima retentativa
-            const nextRetryTime = new Date(Date.now() + delay);
-            const nextRetryTimeISO = nextRetryTime.toISOString();
-
-            // Envia um evento para o EventBridge para agendar a próxima retentativa
-            await this.eventbridgeService.putEvent(
-                { ...dados, nextRetryTime: nextRetryTimeISO }, // Inclui o tempo agendado nos detalhes
-                'agendarRetentativa', // Um novo detail-type para agendamento
-                'api.retentativa.agendamento' // Uma nova source para agendamento
-            );
-        } else {
-            //enviar HSM de falha
-            console.log("Todas as retentativas falharam");
+            // console.error(util.inspect({error}))
+            return await this.eventbridgeService.agendarRetentativa(dados)
         }
     }
 }
