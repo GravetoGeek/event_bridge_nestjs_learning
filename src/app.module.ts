04c43338f145f50f365605a3af8afb7c5ee800b0
gravetogeek <edusrm11@gmail.com>
2025-04-01 22:45:38 -0300

    :recycle: refactor: Refatoração do app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,13 @@
 import {Module} from '@nestjs/common'
 import {AppController} from './app.controller'
 import {AppService} from './app.service'
+import {CommonModule} from './common/common.module'
+import {CustomerModule} from './customer/customer.module'
 import {EventbridgeModule} from './eventbridge/eventbridge.module'
-import {GetCustomerModule} from './get_customer/get_customer.module'
 import {RetentativasModule} from './retentativas/retentativas.module'
 
-
 @Module({
-  imports: [EventbridgeModule, RetentativasModule, GetCustomerModule],
+  imports: [CommonModule,CustomerModule,EventbridgeModule,RetentativasModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,38 @@
+import serverlessExpress from '@codegenie/serverless-express'
 import {NestFactory} from '@nestjs/core'
+import {Callback,Context,Handler} from 'aws-lambda'
 import * as dotenv from 'dotenv'
 import {AppModule} from './app.module'
+import {NestjsLoggerService} from './common/services/logger.service'
+
+
+let server: Handler
 
 async function bootstrap() {
-  dotenv.config({ path: '.env.development' })
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+    dotenv.config({path: '.env.development'})
+    const app=await NestFactory.create(AppModule,{
+        bufferLogs: true,
+    })
+    app.useLogger(app.get(NestjsLoggerService))
+
+    // const globalPrefix = 'api/v1'
+    // app.setGlobalPrefix(globalPrefix)
+
+
+    //   await app.listen(process.env.PORT ?? 3000);
+    await app.init()
+
+    const expressApp=app.getHttpAdapter().getInstance()
+    return serverlessExpress({app: expressApp})
 }
-bootstrap();
+
+export const handler: Handler=async (
+    event: any,
+    context: Context,
+    callback: Callback,
+) => {
+    server=server??(await bootstrap())
+    return server(event,context,callback)
+}
+
+// bootstrap()
